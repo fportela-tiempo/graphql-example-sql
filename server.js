f7dd08d60b3f50e4aa5c6d14547947ee35a4fdda
Flavio Portela Valenzuela <fportela@tiempodevelopment.com>
2017-03-15 15:01:45 -0700

    added cors

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ import express from 'express';
 import schema from './graphql/Schema';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const APP_PORT = 3000;
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:8080',
+}));
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({
     schema: schema
